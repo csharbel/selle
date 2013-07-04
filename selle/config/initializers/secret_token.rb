7a814838d0758341552eec70e6199c4f79f64049
unknown <csharbel@CIN400519L.vanenburg.com>
2013-07-04 11:05:33 +0530

    Initial Commit

---
+++ selle/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selle::Application.config.secret_token = '743397a4f27b95fc1b34086b50176d5cb19b5a32ca16f0a532ce5ae569c5e8dac8921374379df5d9418771772df05b7f47896d8b20d199ca965d41473897e81a'
